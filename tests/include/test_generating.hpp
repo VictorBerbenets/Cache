9751cfb010e6590685dfdcfb85e213a98105e7ae
Victor Berbenets <berbenets.vd@phystech.edu>
2023-08-17 13:27:46 +0300

    made 'weak_perfect' class for testing 'perfect_cache' and tests for it

--- tests/include/test_generating.hpp
+++ tests/include/test_generating.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <list>
 #include <ctime>
 #include <string>
@@ -7,6 +8,7 @@
 namespace Tests {
 using type = std::size_t;
 
+//------------------------------------------------------------------------------------------//
 class weak_lfu {
     using Key = type;
     using cacheType = std::pair<Key, type>;
@@ -75,8 +77,90 @@ weak_lfu::cacheIter weak_lfu::find_minimum_freq() {
     }
     return min_freq;
 }
+//------------------------------------------------------------------------------------------//
 
-class lfu_generator {
+class weak_perfect {
+    using u_int     = std::size_t;
+    using Key       = u_int;
+    using cacheIter = typename std::list<Key>::iterator;
+    
+    cacheIter find(Key key);
+    bool is_full() const noexcept;
+    cacheIter find_farthest_value();
+    void lookup_update();
+public:
+template<typename Iter>
+    weak_perfect(u_int capacity, Iter begin, Iter end);
+    
+    u_int get_hits() const noexcept;
+private:
+    u_int capacity_;
+    u_int cache_size_;
+    std::list<Key> buffer_;
+    std::list<Key> cache_;
+
+    u_int hits_;
+};
+
+template<typename Iter>
+weak_perfect::weak_perfect(u_int capacity, Iter begin, Iter end): 
+               capacity_{capacity}, buffer_(begin, end) {
+    lookup_update();
+}
+
+void weak_perfect::lookup_update() {
+    for (auto buff_it : buffer_) {
+        auto pos_it = find(buff_it);
+        if (pos_it == cache_.end()) {
+            if (is_full()) {
+                auto most_far = find_farthest_value();
+                *most_far = buff_it; 
+            } else {
+                cache_.push_back(buff_it);
+                ++cache_size_;
+            }
+        } else {
+            ++hits_;
+        }
+
+        buffer_.pop_front();
+    }
+}
+
+bool weak_perfect::is_full() const noexcept {
+    return cache_size_ == capacity_;
+}
+
+weak_perfect::cacheIter weak_perfect::find_farthest_value() {
+    auto most_far = buffer_.begin();
+    u_int distance = 0;
+    for (cacheIter it1 = cache_.begin(); it1 != cache_.end(); ++it1) {
+        u_int offset = 0;
+        for (cacheIter it2 = buffer_.begin(); it2 != buffer_.end(); ++it2, ++offset) {
+            if (*it1 == *it2 && offset > distance) {
+                distance = offset;
+                most_far = it1;
+            }
+        }
+    }   
+    return most_far;
+}
+
+weak_perfect::cacheIter weak_perfect::find(Key key) {
+    for (auto iter = cache_.begin(); iter != cache_.end(); ++iter) {
+        if (*iter == key) {
+            return iter;
+        }
+    }
+    return cache_.end();
+}
+
+weak_perfect::u_int weak_perfect::get_hits() const noexcept {
+    return hits_;
+}
+//------------------------------------------------------------------------------------------//
+
+class generator {
     using u_int = std::size_t;
     
     const u_int MAX_CACHE_SIZE = 100;
@@ -87,26 +171,32 @@ class lfu_generator {
 
     const u_int MAX_TESTS_NUMBER = 150;
      
-    void generate_test(u_int test_number);
+    void generate_lfu_files(u_int test_number);
+    void generate_perfect_files(u_int test_number);
 public: 
     void generate(u_int tests_number);
 };
 
-void lfu_generator::generate(u_int test_number) {
+void generator::generate(u_int test_number) {
+    std::srand(std::time(NULL));
+    
     if (test_number > MAX_TESTS_NUMBER) {
         test_number = MAX_TESTS_NUMBER;
     }
     for (u_int count = 0; count < test_number; ++count) {
         std::cout << "count = " << count << '\n';
-        generate_test(count);
+#ifdef __LFU_TEST_GENERATING__
+        generate_lfu_files(u_int test_number);
+#endif
+#ifdef __PERFECT_TEST_GENERATING__
+        generate_perfect_files(u_int test_number);
+#endif
     }
 }
 
-void lfu_generator::generate_test(u_int test_number) {
-    std::srand(std::time(NULL));
-
+void generator::generate_lfu_files(u_int test_number) {
     std::string test_file_name = "test" + std::to_string(test_number) + ".txt";
-    std::ofstream test_file("../resources/" + test_file_name);
+    std::ofstream test_file("../lfu_resources/" + test_file_name);
 
     u_int cache_cap = (std::rand() + 1) % MAX_CACHE_SIZE;
     u_int data_size = (std::rand() * std::rand() + MIN_DATA_SIZE) % MAX_DATA_SIZE;
@@ -121,9 +211,35 @@ void lfu_generator::generate_test(u_int test_number) {
         test_file << ' ' <<  key;
     }
     
-    std::string answ_name = "../resources/" + test_file_name + "_answ" + ".txt";
+    std::string answ_name = "../lfu_resources/" + test_file_name + "_answ" + ".txt";
     std::ofstream answer(answ_name);
     answer << hits;
 }
+
+void generator::generate_perfect_files(u_int test_number) {
+    std::string test_file_name = "test" + std::to_string(test_number) + ".txt";
+    std::ofstream test_file("../perfect_resources/" + test_file_name);
+
+    u_int cache_cap = (std::rand() + 1) % MAX_CACHE_SIZE;
+    u_int data_size = (std::rand() * std::rand() + MIN_DATA_SIZE) % MAX_DATA_SIZE;
+    
+    std::vector<u_int> data{};
+    data.reserve(data_size);
+
+    test_file << cache_cap << ' ' << data_size;
+    for (u_int count = 0; count < data_size; ++count) {
+        u_int key = (std::rand() + MIN_DATA_VALUE) % MAX_DATA_VALUE;
+        data.push_back(key);
+        test_file << ' ' <<  key;
+    }
+
+    weak_perfect cache(cache_cap, data.begin(), data.end());
+   
+    std::string answ_name = "../perfect_resources/" + test_file_name + "_answ" + ".txt";
+    std::ofstream answer(answ_name);
+    answer << cache.get_hits();
+
+}
+//------------------------------------------------------------------------------------------//
 
 };
--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -1,13 +1,13 @@
 #include "test_generating.hpp"
 
 #define __LFU_TEST_GENERATING__
-#define __PERFECT_TEST_GENERATIN__
+#define __PERFECT_TEST_GENERATING__
 
 int main() {
     namespace test = Tests;
 
-#ifdef __LFU_TEST_GENERATING__
-    test::lfu_generator lfu_gen;
+#if defined(__LFU_TEST_GENERATING__) || defined(__PERFECT_TEST_GENERATING__)
+    test::generator gen;
     gen.generate(50);
 #endif
 
