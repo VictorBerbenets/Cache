c3715a11bb914cdd8e02c09be101a1c717c0780d
Victor Berbenets <berbenets.vd@phystech.edu>
2023-08-17 19:59:48 +0300

    replaced list's method: 'push_back(T&&)' to 'emplace_back(args)'

--- Perfect_Caching/include/perfect_cache.hpp
+++ Perfect_Caching/include/perfect_cache.hpp
@@ -28,17 +28,18 @@ class cache {
 template<typename Iter>
     void fill_buffers(Iter first, Iter last);
     void fill_cache();
-    void replace_cache_value(cacheIter cache_iter, std::pair<KeyT, T>&& replacement);
+    void replace_cache_value(cacheIter cache_iter, cacheCell&& replacement);
     void remove_value_entry_number(KeyT key);
-    void push_cache(std::pair<KeyT, T>&& push_value);
+    void push_cache(cacheCell&& push_value);
     cacheIter find_furthest_value();
 public:
     cache(size_type capacity);
     ~cache() = default;
     
     void give_data(std::istream& is);
- template<typename Iter>
+template<typename Iter>
     void give_data(Iter first, Iter last);
+
     bool is_full() const noexcept;
     size_type get_hits() const noexcept;
     void print_cache() const noexcept;
@@ -81,7 +82,7 @@ void cache<T, KeyT>::fill_buffers(std::istream& is, size_type pages_number) {
     page_t<T, KeyT> tmp{};
     for (size_type count = 1; count <= pages_number; ++count) {
         is >> tmp.key_;
-        ordered_buffer_.push_back({tmp.key_, tmp.data_}); //ordered pages 
+        ordered_buffer_.emplace_back(tmp.key_, tmp.data_); //ordered pages 
         //saving page order by page number to std::deque
         unordered_buffer_[tmp.key_].push_back(count);
     }
@@ -94,7 +95,7 @@ void cache<T, KeyT>::fill_buffers(Iter first, Iter last) {
     page_t<T, KeyT> tmp{};
     for (size_type count = 1; first != last; ++first, ++count) {
         tmp.key_ = *first;
-        ordered_buffer_.push_back({tmp.key_, tmp.data_}); //ordered pages 
+        ordered_buffer_.emplace_back(tmp.key_, tmp.data_); //ordered pages 
         //saving page order by page number to std::deque
         unordered_buffer_[tmp.key_].push_back(count);
     }
@@ -141,7 +142,7 @@ typename cache<T, KeyT>::cacheIter cache<T, KeyT>::find_furthest_value() {
 }
 
 template<typename T, typename KeyT>
-void cache<T, KeyT>::replace_cache_value(cacheIter cache_iter, std::pair<KeyT, T>&& replacement) {
+void cache<T, KeyT>::replace_cache_value(cacheIter cache_iter, cacheCell&& replacement) {
     cache_checker_.erase(cache_iter->first);
     *cache_iter = replacement;
 }
@@ -155,7 +156,7 @@ void cache<T, KeyT>::remove_value_entry_number(KeyT key) {
 }
 
 template<typename T, typename KeyT>
-void cache<T, KeyT>::push_cache(std::pair<KeyT, T>&& push_value) {
+void cache<T, KeyT>::push_cache(cacheCell&& push_value) {
     cache_.push_back(push_value);
     ++cache_size_;
 }
@@ -174,6 +175,7 @@ template<typename T, typename KeyT>
 void cache<T, KeyT>::clear() noexcept {
     cache_.clear();
     ordered_buffer_.clear();
+    cache_checker_.clear();
     unordered_buffer_.clear();
     cache_size_ = 0;
     capacity_   = 0;
