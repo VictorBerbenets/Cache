a75540bd669cafd93d9d601a93889dbee22f055e
Victor Berbenets <berbenets.vd@phystech.edu>
2023-08-18 14:36:44 +0300

    renamed namespace in main.cpp

--- Perfect_Caching/src/main.cpp
+++ Perfect_Caching/src/main.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 #include "../include/perfect_cache.hpp"
 
-int main() {
-    namespace pfct = Perfect_Cache;
-    
-    pfct::size_type cache_capacity;
+int main() { 
+    yLAB::size_type cache_capacity;
     std::cin >> cache_capacity;
     if (!std::cin.good()) {
         throw std::runtime_error{"Cache capacity reading error!\n"};
     }
-    pfct::cache<std::string, pfct::size_type> cache(cache_capacity);
+    yLAB::cache<std::string, yLAB::size_type> cache(cache_capacity);
     cache.give_data(std::cin);
 
     std::cout << cache.get_hits() << '\n';
