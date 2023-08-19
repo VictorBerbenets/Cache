3bc4a78b2fafd0a7cca5735125d0a878b68d726d
Victor Berbenets <berbenets.vd@phystech.edu>
2023-08-19 11:48:33 +0300

    added command line argument(number of generating tests) and checking on correct input

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -3,13 +3,22 @@
 
 
 int main(int argc, const char* argv[]) {
-    namespace test = Tests;
+    if (argc != 2) {
+        std::cout << "Incorrect command line arguments! Expected number of tests\n";   
+    }
+    int64_t tests_number{};
+    try {
+        tests_number = std::stoi(argv[1]);
+        if (tests_number < 0) {
+            throw std::invalid_argument{"Got negative number for number of tests\n"};
+        }
+    } catch(std::invalid_argument& inv_arg) {
+        std::cout << inv_arg.what() << std::endl; 
+    }
 
-#if defined(__LFU_TEST_GENERATING__) || defined(__PERFECT_TEST_GENERATING__)
-    std::cout << "AAA";
-    test::generator gen;
-    gen.generate(50);
+#if defined(LFU_TEST_GENERATING_) || defined(PERFECT_TEST_GENERATING_)
+    Tests::generator gen;
+    gen.generate(tests_number);
 #endif
-
 
 }
