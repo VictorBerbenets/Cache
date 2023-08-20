b194babbb0044e4259824c8f6f5a95e6ccf52904
Victor Berbenets <berbenets.vd@phystech.edu>
2023-08-20 19:24:06 +0300

    first version of tests generator

--- tests/test_runner.sh
+++ tests/test_runner.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 #color atributes
-red="x1b[31m"
-green="x1b[32m"
-usual="x1b[0m"
+red="\033[1;31m"
+green="\033[1;32m"
+blue="\033[1;36m"
+purple="\033[1;35m"
+white="\033[1;37m"
+usual="\033[0m"
 
 lfu_tests_dir="lfu_resources/"
 lfu_tests="${lfu_tests_dir}tests/"
@@ -12,61 +15,60 @@ lfu_ans="${lfu_tests_dir}answers/"
 lfu_cache_out="./../LFU_Cache/build/lfu_cache"
 
 perfect_tests_dir="perfect_resources/"
-perfect_tests="{perfect_tests_dir}tests/"
-perfect_ans="{perfect_tests_dir}answers/"
+perfect_tests="${perfect_tests_dir}tests/"
+perfect_ans="${perfect_tests_dir}answers/"
 
 perfect_cache_out="./../Perfect_Caching/build/perfect_cache"
 
 function run_tests {
-    echo "generating tests..."
+    echo -e "${blue}generating tests...${usual}"
     cd build
     make
     ./testing $tests_number
     cd ../
-    echo "done"
-
-    echo "tests number = $tests_number"
-    
+    echo -e "${blue}done${usual}"
+ 
     touch compare_file
     if [ $is_lfu = "true" ]
     then
-        echo "lfu tests:"
+        echo -e "${white}lfu tests:${usual}"
         for ((i = 1; i <= $tests_number; ++i))
         do  
-                $lfu_cache_out < ${lfu_tests}test${i}.txt > $compare_file  
-                echo -n "Test ${i}: "
-                if diff -w ${lfu_ans}answ${i}.txt compare_file
+                $lfu_cache_out < ${lfu_tests}test${i}.txt > compare_file
+                echo -n -e "${purple}Test ${i}: ${usual}"
+                if diff -w ${lfu_ans}answ${i}.txt compare_file &>/dev/null
                 then
-                    echo echo -e "${red}failed${usual}"
+                    echo -e "${green}passed${usual}" 
                 else
-                    echo echo -e "${green}passed${usual}"
+                    echo -e "${red}failed${usual}"
                 fi
+                cat compare_file
         done
     fi
     if [ $is_perfect = "true" ]
     then
-        echo "perfect tests:"
+        echo -e "${white}perfect tests:${usual}"
         for ((i = 1; i <= $tests_number; ++i))
         do  
-                $perfect_cache_out < ${perfect_tests}test${i}.txt > $compare_file  
-                echo -n "Test ${i}: "
-                if diff -w ${perfect_ans}answ${i}.txt compare_file
+                $perfect_cache_out < ${perfect_tests}test${i}.txt > compare_file  
+                if diff -w ${perfect_ans}answ${i}.txt compare_file &>/dev/null
                 then
-                    echo echo -e "${red}failed${usual}"
+                    echo -e "${green}passed${usual}"
                 else
-                    echo echo -e "${green}passed${usual}"
+                    echo -e "${red}failed${usual}"
                 fi
+                cat compare_file
         done
     fi
 }
 
 function build_caches {
-    echo "Building caches..."
+    echo -e "${blue}Building caches...${usual}"
     cmake -S ./ -B build/
     cd build/
     make
     cd ..
-    echo "done"
+    echo -e "${blue}done${usual}"
 }
 
 #main part
@@ -77,7 +79,7 @@ build_caches
 
 if [ $# -lt 2 ] || [ $# -gt 3 ]
 then
-    echo "invalid number of arguments: $#. Expected 2 or 3"
+    echo -e "${red}invalid number of arguments:${usual} $#. Expected 2 or 3"
 #if we have two args(cache name and number of tests)
 elif [ $# -eq 2 ]
 then
@@ -89,7 +91,7 @@ then
     then
         is_perfect="true"
     else
-        echo "unknown command $1"
+        echo -e "${red}unknown command:${usual} $1"
     fi
 #if we have three args(cache name1(2) cache name2(1) and number of tests)
 else
@@ -103,7 +105,7 @@ else
         is_perfect="true"
         is_lfu="true"
     else
-        echo "invalid set of cache's names : $1 $2"
+        echo -e "${red}invalid set of cache's names :${usual} $1 $2"
     fi
 fi
 
