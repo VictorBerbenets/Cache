383b799df2147caf6286facae8f885192fd4ded5
Victor Berbenets <berbenets.vd@phystech.edu>
2023-08-22 14:26:41 +0300

    changed skript's logic

--- tests/test_runner.sh
+++ tests/test_runner.sh
@@ -12,29 +12,30 @@ lfu_tests_dir="lfu_resources/"
 lfu_tests="${lfu_tests_dir}tests/"
 lfu_ans="${lfu_tests_dir}answers/"
 
-lfu_cache_out="./../LFU_Cache/build/lfu_cache"
+lfu_cache_out="./../build/lfu"
 
 perfect_tests_dir="perfect_resources/"
 perfect_tests="${perfect_tests_dir}tests/"
 perfect_ans="${perfect_tests_dir}answers/"
 
-perfect_cache_out="./../Perfect_Caching/build/perfect_cache"
+perfect_cache_out="./../build/perfect"
 
 function run_tests {
     echo -e "${blue}generating tests...${usual}"
     cd build
-    make
+    cmake --build .
     ./testing $tests_number
     cd ../
     echo -e "${blue}done${usual}"
  
     touch compare_file
-    if [ $is_lfu = "true" ]
+    #if lfu tests dir is empty
+    if ! [ `ls ${lfu_tests} | wc -l` -eq 0 ]
     then
         echo -e "${white}lfu tests:${usual}"
-        for ((i = 1; i <= $tests_number; ++i))
+        for file in ${lfu_tests}*
         do  
-                $lfu_cache_out < ${lfu_tests}test${i}.txt > compare_file
+                $lfu_cache_out < file > compare_file
                 echo -n -e "${purple}Test ${i}: ${usual}"
                 if diff -w ${lfu_ans}answ${i}.txt compare_file &>/dev/null
                 then
@@ -46,12 +47,15 @@ function run_tests {
                 cat compare_file
         done
     fi
-    if [ $is_perfect = "true" ]
+    #if perfect tests dir is empty
+    if ! [ `ls ${perfect_tests} | wc -l` -eq 0 ]
+
     then
         echo -e "${white}perfect tests:${usual}"
-        for ((i = 1; i <= $tests_number; ++i))
-        do  
-                $perfect_cache_out < ${perfect_tests}test${i}.txt > compare_file  
+        for file in ${perfect_tests}*
+        do      
+                echo "file = ${file}"
+                $perfect_cache_out < file > compare_file  
                 echo -n -e "${purple}Test ${i}: ${usual}"
                 if diff -w ${perfect_ans}answ${i}.txt compare_file &>/dev/null
                 then
@@ -74,46 +78,21 @@ function build_caches {
     echo -e "${blue}done${usual}"
 }
 
+
 #main part
 is_lfu="false"
 is_perfect="false"
 error="false"
+tests_number=0
 
 build_caches
 
-if [ $# -lt 2 ] || [ $# -gt 3 ]
+if [ $# -ne 1 ]
 then
     error="true"
-    echo -e "${red}invalid number of arguments:${usual} $#. Expected 2 or 3"
-#if we have two args(cache name and number of tests)
-elif [ $# -eq 2 ]
-then
-    tests_number=$2
-    if [ $1 = "lfu" ]
-    then
-        is_lfu="true"
-    elif [ $1 = "perfect" ]
-    then
-        is_perfect="true"
-    else
-        error="true"
-        echo -e "${red}unknown command:${usual} $1"
-    fi
-#if we have three args(cache name1(2) cache name2(1) and number of tests)
+    echo -e "${red}invalid number of arguments:${usual} $#. Expected 1, got ${$#}"
 else
-    tests_number="$3"
-    if [ $1 = "lfu" ] && [ $2 = "perfect" ]
-    then
-        is_lfu="true"
-        is_perfect="true"
-    elif [ $1 = "perfect" ] && [ $2 = "lfu" ]
-    then
-        is_perfect="true"
-        is_lfu="true"
-    else
-        error="true"
-        echo -e "${red}invalid set of cache's names :${usual} $1 $2"
-    fi
+    tests_number=$1
 fi
 
 if [ $error = "false" ]
