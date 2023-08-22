5f4ba80bc3ca6777aa740d2429d28e8491ab3397
Victor Berbenets <berbenets.vd@phystech.edu>
2023-08-22 19:49:14 +0300

    deleted key_ in struct item(was unusing), cache cap >= 1 now

--- LFU_Cache/include/lfu_cache.hpp
+++ LFU_Cache/include/lfu_cache.hpp
@@ -18,17 +18,19 @@ public:
     using size_type = std::size_t;  
     using page_t    = std::pair<T, KeyT>;
 private:
+    static constexpr size_type MIN_CAPACITY = 1;
+
     struct item;
     struct frequencyItem;
     
     using freqIter = typename std::list<frequencyItem>::iterator;
     using itemIter = typename std::list<item>::iterator;  
     
-    void insert_item(KeyT key, const page_t& value, freqIter iter);
+    void insert_item(const page_t& value, freqIter iter);
     void remove_last_item();
 public:
     explicit lfu_cache(size_type capacity):
-                    capacity_{capacity} {};
+                    capacity_{std::max(MIN_CAPACITY, capacity)} {};
     ~lfu_cache() = default;
 
     bool is_full() const noexcept;
@@ -40,12 +42,11 @@ private:
     std::list<frequencyItem> cache_;
 
     struct item {
-        KeyT key_;
         page_t value_;
         freqIter fr_iter_;
         //constructor for list emplace() method    
-        item(KeyT key, const page_t& value, freqIter it):
-            key_{key}, value_{value}, fr_iter_{it} {};
+        item(const page_t& value, freqIter it):
+            value_{value}, fr_iter_{it} {};
     };
     
     struct frequencyItem {
@@ -59,9 +60,6 @@ private:
 
 template<typename T, typename KeyT>
 bool lfu_cache<T, KeyT>::lookup_update(KeyT key, const page_t& value) {
-    if (!capacity_) {
-        return false;
-    }
     auto is_found = hash_table_.find(key);
     if (is_found == hash_table_.end()) {
         if (is_full()) {    
@@ -72,11 +70,11 @@ bool lfu_cache<T, KeyT>::lookup_update(KeyT key, const page_t& value) {
             }
         } else {
             //if cache has't freq-list with freq_ == 1
-            if (cache_.begin()->freq_ != 1 || !hash_table_.size()) {
+            if (cache_.begin()->freq_ != 1 || hash_table_.empty()) {
                 cache_.emplace_front(1);                
             }
         }
-        insert_item(key, value, cache_.begin());
+        insert_item(value, cache_.begin());
         return false;
     } 
    //element was found in hash_table //
@@ -86,10 +84,10 @@ bool lfu_cache<T, KeyT>::lookup_update(KeyT key, const page_t& value) {
     (cache_iter->freq_list_).erase(is_found->second);//erase item on found key
     //looking for next suitable frequencyItem: if next one isn't suitable than insert your own
     if (next_cache_iter != cache_.end() && next_cache_iter->freq_ == (cache_iter->freq_ + 1)) {
-        insert_item(key, value, next_cache_iter);
+        insert_item(value, next_cache_iter);
     } else {
         freqIter inserted_cell = cache_.emplace(next_cache_iter, cache_iter->freq_ + 1); 
-        insert_item(key, value, inserted_cell);
+        insert_item(value, inserted_cell);
     }
     if ((cache_iter->freq_list_).empty()) {
         cache_.erase(cache_iter);
@@ -103,17 +101,17 @@ bool lfu_cache<T, KeyT>::is_full() const noexcept {
 }
 
 template<typename T, typename KeyT>
-void lfu_cache<T, KeyT>::insert_item(KeyT key, const page_t& value, freqIter new_freq_iter) {
+void lfu_cache<T, KeyT>::insert_item(const page_t& value, freqIter new_freq_iter) {
     auto& new_freq_list = new_freq_iter->freq_list_;
-    new_freq_list.emplace_front(key, value, new_freq_iter);  //push new item in the frequencyItem-list                        
-    hash_table_[key] = new_freq_list.begin(); //saving new item iter                                                            
+    new_freq_list.emplace_front(value, new_freq_iter);  //push new item in the frequencyItem-list                        
+    hash_table_[value.second] = new_freq_list.begin(); //saving new item iter                                                            
 }
 
 template<typename T, typename KeyT>
 void lfu_cache<T, KeyT>::remove_last_item() {
     //remove last element in the first freq-list node
     auto& freq_list = cache_.begin()->freq_list_;
-    auto key_to_remove = std::prev(freq_list.end())->key_;
+    auto key_to_remove = std::prev(freq_list.end())->value_.second;
     hash_table_.erase(key_to_remove);
     freq_list.pop_back();
     if (freq_list.empty()) {
@@ -127,7 +125,7 @@ void lfu_cache<T, KeyT>::print_cache() const {
     for (const auto& it1 : cache_) {
         std::cout << it1.freq_ << ":";
         for (const auto& it2 : it1.freq_list_) {
-            std::cout << it2.key_ << ' ';
+            std::cout << it2.value.second << ' ';
         }
         std::cout << '\n';
     }
