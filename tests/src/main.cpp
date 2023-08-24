ea548b35974f3eecc78ef9f889f93d2e170263da
Victor Berbenets <berbenets.vd@phystech.edu>
2023-08-24 12:10:47 +0300

    improved code

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -5,8 +5,7 @@ int main(int argc, const char* argv[]) {
         throw std::runtime_error{"Incorrect command line arguments! Expected number of tests\n"};
     }
 
-    int tests_number{};
-    tests_number = std::stoi(argv[1]);
+    int tests_number = std::stoi(argv[1]);
     if (tests_number < 0) {
         throw std::invalid_argument{"Got negative number for number of tests\n"};
     }
